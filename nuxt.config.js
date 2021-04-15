09741b932c113b4636ab8c117e686231b3f613cd
tansionline <tatarselcuk27@gmail.com>
2021-04-15 11:38:50 +0300

    firebase setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,22 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: 'AIzaSyA4Y4Rsh1ClDiRHiaF3W2tnTM-6FMHP-5c',
+          authDomain: 'entrylevel-679fb.firebaseapp.com',
+          projectId: 'entrylevel-679fb',
+          storageBucket: 'entrylevel-679fb.appspot.com',
+          messagingSenderId: '247921238998',
+          appId: '1:247921238998:web:b78ba1b878788c941efa77',
+        },
+        services: {
+          auth: true, // Just as example. Can be any other service.
+        },
+      },
+    ],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
