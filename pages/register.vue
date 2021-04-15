a6a8d3c5177343b5a7b529c293d1bd8e05b8d54f
tansionline <tatarselcuk27@gmail.com>
2021-04-15 22:16:54 +0300

    background image added

--- pages/register.vue
+++ pages/register.vue
@@ -24,29 +24,68 @@ export default {
 
 <template>
   <div class="container-fluid">
-    <div>
-      <h1>Register</h1>
-      <b-form @submit.prevent="register">
-        <b-form-group label="Email address:" label-for="email">
-          <b-form-input
-            id="email"
-            v-model="email"
+    <div class="container">
+      <div class="register-card">
+        <h1>Register</h1>
+        <form>
+          <label for="email">Email:</label>
+          <input
             type="email"
+            v-model="email"
+            name="email"
             placeholder="cagatay@cali.com"
-            required
-          ></b-form-input>
-        </b-form-group>
+          />
 
-        <b-form-group label="Password:" label-for="password">
-          <b-form-input
-            v-model="password"
-            placeholder="*******"
+          <label for="password">Password:</label>
+          <input
             type="password"
-            required
-          ></b-form-input>
-        </b-form-group>
-        <b-button type="submit" variant="primary">Register</b-button>
-      </b-form>
+            v-model="password"
+            name="password"
+            placeholder="***********"
+          />
+          <a href="/login">Have you account</a>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+input[type='email'],
+input[type='password'] {
+  width: 100%;
+  padding: 0.5em 1.5em;
+  margin: 0.4em;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type='submit'] {
+  width: 100%;
+  background-color: #4caf50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 0.2em;
+  cursor: pointer;
+}
+
+input[type='submit']:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  padding: 20px;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
